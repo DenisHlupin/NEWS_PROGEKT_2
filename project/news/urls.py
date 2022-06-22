c0b8326dc4d80077af786f5098fdcc21c54f17d8
DenisHlupin <102675007+DenisHlupin@users.noreply.github.com>
2022-06-22 14:29:35 +0300

    Delete project directory

--- project/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PostsList, PostsDetail
-
-urlpatterns = [
-    path('', PostsList.as_view()),
-    path('<int:pk>', PostsDetail.as_view(), name='post_detail'),
-
-]
